b86c9982291e676478837f6dc2e0c040c14f80a0
Ghennadii Dovgani <dovgani@comcast.net>
2020-04-25 16:47:04 -0400

    Change name to gd-controls-example

--- projects/gd-controls-example/e2e/src/app.e2e-spec.ts
+++ projects/gd-controls-example/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('controls-example app is running!');
+    expect(page.getTitleText()).toEqual('gd-controls-example app is running!');
   });
 
   afterEach(async () => {
--- projects/gd-controls-example/karma.conf.js
+++ projects/gd-controls-example/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/controls-example'),
+      dir: require('path').join(__dirname, '../../coverage/gd-controls-example'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
